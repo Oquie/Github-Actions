f3c1c51dac95b9522831ff3a601d7fe03731c251
oquie <stellanwachukwu@gmail.com>
2023-03-29 21:41:18 -0400

    new commit

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ provider "aws" {
     region = "us-east-1"
   }
   resource "aws_vpc" "actions" {
-    cidr_block = "10.0.0.0/16"    
-  }
- /* tags = {
+    cidr_block = "10.10.0.0/24"    
+   tags = {
     Name = "class30"
-  }*/
+  }
+}
